63f340c32e1437ed1a1134d657aaea6c80e82cbe
Daniel M. Weeks <weeksd2@rpi.edu>
2013-12-06 13:05:54 -0500

    Add CPU-hour quota field to cluster access

--- schema/ProjectClusterAccess.sql
+++ schema/ProjectClusterAccess.sql
@@ -4,6 +4,7 @@ CREATE TABLE project_cluster_access (
     snuuid bigint NOT NULL,
     share bigint default 0,
     parentnode character varying(16),
+    cpuquota int,
     PRIMARY KEY (snuuid, projid, cluster),
     FOREIGN KEY (snuuid, projid) REFERENCES projects(snuuid, projid) ON UPDATE CASCADE,
     FOREIGN KEY (snuuid, cluster, parentnode) REFERENCES cluster_sharing(snuuid, cluster, node) ON UPDATE CASCADE
